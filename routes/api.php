464ca2cc78e7c1bc088e61f698e12f97477b02c2
koshala manojeewa <koshala.manojeewa@silverstripers.com>
2020-04-29 15:36:28 +0530

    aticle routes and controller changes

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,12 @@ Route::group(['midleware' => 'auth:api'], function() {
     Route::post('details', 'API\UserController@details');
 });
 
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
+
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
